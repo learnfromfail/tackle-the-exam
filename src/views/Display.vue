1ff4e902fe8f98dc0df0a66047f8d1ae95e761f2
erickwok HomePC <learnfromfail@gmail.com>
2021-05-19 22:50:26 +0800

    19/5 commit

--- src/views/Display.vue
+++ src/views/Display.vue
@@ -1,9 +1,11 @@
 <template>
-    <!-- <iframe style="" src="../notes/maths/1.html"></iframe> -->
-    <iframe src="@/notes/maths/1.html" width="50%" height="800px" frameborder="1" scrolling="auto" id="mainIframe" ref="mainIframe" name="mainIframe"></iframe>
+  <div></div>
 </template>
 <script>
 export default {
     name: "Display",
+    props:{
+
+    }
 }
 </script>
