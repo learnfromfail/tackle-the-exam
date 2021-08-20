8fd534909961c113a0e0c00ab783ad72fdc85286
Eric Kwok <learnfromfail@gmail.com>
2021-08-20 21:26:55 +0800

    finally succed in using mathjax

--- src/main.js
+++ src/main.js
@@ -3,10 +3,24 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
+// createApp.config.productionTip = false;
+
+import MathJax, { initMathJax, renderByMathjax } from 'mathjax-vue'
 //const VueMathjax = require('vue-mathjax');
 //import  VueMathjax from "vue-mathjax";
 
+function onMathJaxReady() {
+  // The parent node of need to be rendered into the formula node set
+  const el = document.getElementById('elementId')
+  // ❗️❗️ When there is no el will begin to render the default root node
+  renderByMathjax(el)
+}
+
+initMathJax({}, onMathJaxReady)
+
 createApp(App)
   .use(router)
-  .mount("#app");
+  .mount("#app")
+  .use(MathJax);
   //.use(VueMathjax);
--- src/views/notes/m2.vue
+++ src/views/notes/m2.vue
@@ -7,7 +7,13 @@
     </p>
     <button>1234</button>
     <textarea v-model="formula" cols="30" rows="10"></textarea>
-    <vue-mathjax :formula="formula"></vue-mathjax>
+    123
+    <!-- <vue-mathjax :formula="formula">555</vue-mathjax> -->
+  <math-jax :latex="formula" />
+  <!-- <math-jax :latex="formula" :block="true" /> -->
+    456
+    <math-jax :latex="formula" />
+    <div>{{formula}}</div>
   </div>
 </template>
 
@@ -15,28 +21,45 @@
 <script>
  //import LeftMenu from "../../components/LeftMenu";
 //const VueMathjax = require('vue-mathjax');
-import  VueMathjax from "vue-mathjax";
+// import MathJax from 'mathjax-vue' 
+// import MathJax from '../../main'
 
 export default {
   name: "M2",
   components:{
     // LeftMenu
-    'vue-mathjax': VueMathjax
+    // MathJax
+    // math-jax  : MathJax,
   },
   props: {},
   data() {
     return {
       content: "",
-      formula: '$$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$'
-
+      formula: '$$x = {-b \\pm \\sqrt{b^2-4ac}\\sqrt{b^2-4ac} \\over 2a}.$$'
     };
   },
-  methods: {},
+  methods: {
+    //   reRender() {
+    //   if(window.MathJax) {
+    //     console.log('rendering mathjax');
+    //     window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub], () => console.log('done'));
+    //   }
+    // }
+  },
   created() {
     this.content =[
-      "Comming soon √a/√b =√(a/b) "
+      "Comming soon √a/√b =√(a/b)  123"
     ];
   },
+  mounted() {
+    // this.reRender();
+  },
+  watch: {
+    // formula: function() {
+    //   console.log('data changed')
+    //   this.reRender();
+    // }
+  }
 };
 </script>
 <style scoped>
